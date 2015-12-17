dd519790cfe9232d346e9fea523c6fe1715982a6
Rabea Gleissner <rabeagleissner@Rabea.default>
2015-12-17 18:57:45 +0000

    knows if there is a winner

--- src/main/java/de/rabea/Rules.java
+++ src/main/java/de/rabea/Rules.java
@@ -3,6 +3,7 @@ package de.rabea;
 public class Rules {
 
     private final Board board;
+    private final int[][] WINNING_COMBINATIONS = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public Rules(Board board) {
         this.board = board;
@@ -10,10 +11,22 @@ public class Rules {
     }
 
     public boolean gameOver() {
-        return true;
+        return board.isFull();
     }
 
     public boolean winner() {
+        Cell[] gameState = gameState();
+        for (int[] combos : WINNING_COMBINATIONS) {
+            if (gameState[combos[0]] == gameState[combos[1]] &&
+                    gameState[combos[1]] == gameState[combos[2]] &&
+                    gameState[combos[2]] != Cell.EMPTY) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private Cell[] gameState() {
+        return board.returnCells();
     }
 }
--- src/test/java/de/rabea/RulesTest.java
+++ src/test/java/de/rabea/RulesTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static de.rabea.Cell.*;
 
 public class RulesTest {
 
@@ -15,10 +16,90 @@ public class RulesTest {
         board = new Board();
         rules = new Rules(board);
     }
+
     @Test
     public void gameOverWithoutWinner() {
+        rules = new Rules(new FullBoard());
         assertTrue(rules.gameOver());
-        assertFalse(rules.winner());
     }
 
+    @Test
+    public void verticalWinningCombination() {
+        rules = new Rules(new VerticalWinningBoard());
+        assertTrue(rules.winner());
+
+    }
+
+    @Test
+    public void horizontalWinningCombination() {
+        rules = new Rules(new HorizontalWinningBoard());
+        assertTrue(rules.winner());
+    }
+
+    @Test
+    public void diagonalWinningBoard() {
+        rules = new Rules(new DiagonalWinningBoard());
+        assertTrue(rules.winner());
+    }
+
+    public class DiagonalWinningBoard extends Board {
+        private Cell[] cells;
+        public DiagonalWinningBoard() {
+            this.cells = new Cell[]{X, X, O,
+                                EMPTY, X, EMPTY,
+                                EMPTY, O, X};
+        }
+        @Override
+        public Cell[] returnCells() {
+            return cells;
+        }
+    }
+
+    public class HorizontalWinningBoard extends Board {
+        private Cell[] cells;
+        public HorizontalWinningBoard() {
+            this.cells = new Cell[]{X, X, X,
+                                EMPTY, EMPTY, EMPTY,
+                                    EMPTY, O, O};
+        }
+        @Override
+        public Cell[] returnCells() {
+            return cells;
+        }
+    }
+
+    public class FullBoard extends Board {
+        private Cell[] cells;
+
+        public FullBoard() {
+            this.cells = new Cell[]{X, X, O,
+                                    O, X, O,
+                                    X, O, X};
+        }
+        @Override
+        public Cell[] returnCells() {
+            return cells;
+        }
+        @Override
+        public boolean isFull() {
+            return true;
+        }
+
+    }
+
+    public class VerticalWinningBoard extends Board {
+        private Cell[] cells;
+
+        public VerticalWinningBoard() {
+            this.cells = new Cell[]{X, X, O,
+                                    X, O, O,
+                                    X, O, X};
+        }
+
+        @Override
+        public Cell[] returnCells() {
+            return cells;
+        }
+
+    }
 }
