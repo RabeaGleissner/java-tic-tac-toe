e66b9f88edc771ab1351413cdd0c69acbef1754b
Rabea Gleissner <rabeagleissner@Rabea.default>
2015-12-18 17:02:57 +0000

    displays board to user

---
+++ src/main/java/de/rabea/UserInterface.java
@@ -0,0 +1,25 @@
+package de.rabea;
+
+public class UserInterface {
+
+    public String displayBoard(Cell[] cells) {
+        String boardPrinter = "";
+        int i = 0;
+        for (Enum cell : cells) {
+            i ++;
+
+            if (cell == Cell.EMPTY) {
+                boardPrinter += i;
+            } else {
+                boardPrinter += cell.toString();
+            }
+            
+            boardPrinter += " ";
+
+            if (i%3 == 0) {
+                boardPrinter += "\n";
+            }
+        }
+        return boardPrinter;
+    }
+}
---
+++ src/test/java/de/rabea/UserInterfaceTest.java
@@ -0,0 +1,31 @@
+package de.rabea;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserInterfaceTest {
+
+    private UserInterface userInterface;
+
+    @Before
+    public void setup() {
+        userInterface = new UserInterface();
+
+    }
+
+    @Test
+    public void displaysEmptyBoard() {
+        Board board = new Board();
+        assertThat(userInterface.displayBoard(board.returnCells())).isEqualTo("1 2 3 \n4 5 6 \n7 8 9 \n");
+    }
+
+    @Test
+    public void displaysBoardWithMarks() {
+        Board board = new Board();
+        board.placeMark(1, Cell.X);
+        board.placeMark(3, Cell.O);
+        assertThat(userInterface.displayBoard(board.returnCells())).isEqualTo("1 X 3 \nO 5 6 \n7 8 9 \n");
+    }
+}
