002fa2283ad559a33a144be36faa09cd391e5bc9
Rabea Gleissner <rabeagleissner@Rabea.default>
2015-12-21 11:56:28 +0000

    implements real console print method

--- src/main/java/de/rabea/RealConsole.java
+++ src/main/java/de/rabea/RealConsole.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RealConsole implements Console {
 
     public void print(String message) {
-
+        System.out.println(message);
     }
 
     public String readUserInput() {
