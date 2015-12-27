17f4930427c68e6bc10c5e31451148e92656fadb
Rabea Gleissner <rabeagleissner@gmail.com>
2015-12-27 14:53:17 +0100

    fixed indentation

--- src/main/java/de/rabea/App.java
+++ src/main/java/de/rabea/App.java
@@ -4,12 +4,9 @@ import de.rabea.game.Game;
 import de.rabea.ui.RealConsole;
 import de.rabea.ui.UserInterface;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         Game game = new Game(new UserInterface(new RealConsole()));
         game.play();
-
     }
 }
