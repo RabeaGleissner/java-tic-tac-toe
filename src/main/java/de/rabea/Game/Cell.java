034a18e8892324741ac3bf0b129a9266bb7f9cb3
Rabea Gleissner <rabeagleissner@gmail.com>
2016-01-15 12:26:51 +0000

    deletes unnecessary method

--- src/main/java/de/rabea/Game/Cell.java
+++ src/main/java/de/rabea/Game/Cell.java
@@ -3,15 +3,5 @@ package de.rabea.game;
 public enum Cell {
     X,
     O,
-    EMPTY;
-
-    public Mark convertToMark() {
-        Mark mark;
-        if (this == Cell.X) {
-            mark = Mark.X;
-        } else {
-            mark = Mark.O;
-        }
-        return mark;
-    }
+    EMPTY
 }
