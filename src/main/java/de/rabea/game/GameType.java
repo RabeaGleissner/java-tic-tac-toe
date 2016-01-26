6519ad0856c2e31e8a3be902d9945f50d27cb628
Rabea Gleissner <rabeagleissner@gmail.com>
2016-01-26 17:33:04 +0000

    shortened code

--- src/main/java/de/rabea/game/GameType.java
+++ src/main/java/de/rabea/game/GameType.java
@@ -4,6 +4,7 @@ import de.rabea.player.HumanPlayer;
 import de.rabea.player.UnbeatableComputerPlayer;
 import de.rabea.ui.UserInterface;
 
+import static de.rabea.game.GameMode.*;
 import static de.rabea.game.Mark.O;
 import static de.rabea.game.Mark.X;
 
@@ -20,58 +21,22 @@ public class GameType {
         GameMode gameMode = userInterface.getGameModeFromUser();
         userInterface.announceMarkDistribution(gameMode);
 
-        if (isHumanVsComputer(gameMode)) {
-            return humanVsComputerGame();
-        } else if (isComputerVsHumanGame(gameMode)) {
-            return computerVsHumanGame();
-        } else if (isComputerVsComputerGame(gameMode)) {
-            return computerVsComputerGame();
+        return new Game(userInterface, createPlayer(gameMode), createOpponent(gameMode), gameManager);
+    }
+
+    private Player createPlayer(GameMode gameMode) {
+        if (gameMode == HvH || gameMode == HvC) {
+           return new HumanPlayer(userInterface, X);
         } else {
-            return humanVsHumanGame();
+           return new UnbeatableComputerPlayer(X);
         }
     }
 
-    private Game humanVsHumanGame() {
-        return new Game(userInterface, newHumanPlayer(), newHumanOpponent(), gameManager);
-    }
-
-    private Game humanVsComputerGame() {
-        return new Game(userInterface, newHumanPlayer(), newComputerOpponent(), gameManager);
-    }
-
-    private Game computerVsHumanGame() {
-        return new Game(userInterface, newComputerPlayer(), newHumanOpponent(), gameManager);
-    }
-
-    private Game computerVsComputerGame() {
-        return new Game(userInterface, newComputerPlayer(), newComputerOpponent(), gameManager);
-    }
-
-    private HumanPlayer newHumanPlayer() {
-        return new HumanPlayer(userInterface, X);
-    }
-
-    private HumanPlayer newHumanOpponent() {
-        return new HumanPlayer(userInterface, O);
-    }
-
-    private UnbeatableComputerPlayer newComputerPlayer() {
-        return new UnbeatableComputerPlayer(X);
-    }
-
-    private UnbeatableComputerPlayer newComputerOpponent() {
-        return new UnbeatableComputerPlayer(O);
-    }
-
-    private boolean isHumanVsComputer(GameMode gameMode) {
-        return gameMode == GameMode.HvC;
-    }
-
-    private boolean isComputerVsComputerGame(GameMode gameMode) {
-        return gameMode == GameMode.CvC;
-    }
-
-    private boolean isComputerVsHumanGame(GameMode gameMode) {
-        return gameMode == GameMode.CvH;
+    private Player createOpponent(GameMode gameMode) {
+        if (gameMode == HvC || gameMode == CvC) {
+            return new UnbeatableComputerPlayer(O);
+        } else {
+            return new HumanPlayer(userInterface, O);
+        }
     }
 }
