cad8ceb62682cbfb0f3ed3ee1b5018ef98772dc4
Rabea Gleissner <rabeagleissner@gmail.com>
2016-01-27 17:02:37 +0000

    creates PlayerFactory

---
+++ src/test/java/de/rabea/player/PlayerFactoryTest.java
@@ -0,0 +1,53 @@
+package de.rabea.player;
+
+import de.rabea.game.Console;
+import de.rabea.game.GameMode;
+import de.rabea.game.Setup;
+import de.rabea.ui.FakeConsole;
+import de.rabea.ui.FakeUserInterface;
+import org.junit.Before;
+import org.junit.Test;
+
+import static de.rabea.game.Mark.O;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerFactoryTest {
+    FakeUserInterface fakeUserInterface;
+    FakeComputerPlayer fakeComputerPlayer;
+    Setup setup;
+    Console fakeConsole;
+    PlayerFactory playerFactory;
+
+    @Before
+    public void setup() {
+        fakeUserInterface = new FakeUserInterface();
+        fakeComputerPlayer = new FakeComputerPlayer(O);
+        setup = new Setup(fakeUserInterface, playerFactory);
+        fakeConsole = new FakeConsole();
+        playerFactory = new PlayerFactory(fakeUserInterface);
+    }
+
+    @Test
+    public void createsAHumanPlayerForAHumanVsComputerGame() {
+        PlayerFactory playerFactory = new PlayerFactory(fakeUserInterface);
+        assertTrue(playerFactory.createPlayer(GameMode.HumanVsComputer) instanceof HumanPlayer);
+    }
+
+    @Test
+    public void createsAComputerPlayerForAHumanVsComputerGame() {
+        PlayerFactory playerFactory = new PlayerFactory(fakeUserInterface);
+        assertTrue(playerFactory.createOpponent(GameMode.HumanVsComputer) instanceof UnbeatableComputerPlayer);
+    }
+
+    @Test
+    public void createsAComputerPlayerForAComputerVsHumanGame() {
+        PlayerFactory playerFactory = new PlayerFactory(fakeUserInterface);
+        assertTrue(playerFactory.createPlayer(GameMode.ComputerVsHuman) instanceof UnbeatableComputerPlayer);
+    }
+
+    @Test
+    public void createsAHumanPlayerForAComputerVsHumanGame() {
+        PlayerFactory playerFactory = new PlayerFactory(fakeUserInterface);
+        assertTrue(playerFactory.createOpponent(GameMode.ComputerVsHuman) instanceof HumanPlayer);
+    }
+}
