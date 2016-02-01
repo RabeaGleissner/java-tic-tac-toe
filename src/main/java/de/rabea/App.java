605c4e78ac5e68079d641f1fc0113c344997811d
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-01 11:36:58 +0000

    adjusts other tests and app.java according to refactoring necessary for testing IOException

--- src/main/java/de/rabea/App.java
+++ src/main/java/de/rabea/App.java
@@ -5,9 +5,13 @@ import de.rabea.player.PlayerFactory;
 import de.rabea.ui.RealConsole;
 import de.rabea.ui.UserInterface;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main( String[] args ) {
-        UserInterface userInterface = new UserInterface(new RealConsole(System.in, System.out));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        UserInterface userInterface = new UserInterface(new RealConsole(bufferedReader, System.out));
         PlayerFactory playerFactory = new PlayerFactory(userInterface);
         new Game(userInterface, playerFactory).startApplication();
     }
--- src/test/java/de/rabea/ui/RealConsoleTest.java
+++ src/test/java/de/rabea/ui/RealConsoleTest.java
@@ -9,29 +9,31 @@ import static org.junit.Assert.assertEquals;
 public class RealConsoleTest {
 
 
-//    @Test
-//    public void readsAGivenStringFromTheConsole() {
-//        InputStream input = new ByteArrayInputStream("hello\n".getBytes());
-//        RealConsole realConsole = new RealConsole(input, null);
-//        assertEquals("hello", realConsole.readUserInput());
-//    }
+    @Test
+    public void readsAGivenStringFromTheConsole() {
+        InputStream input = new ByteArrayInputStream("hello\n".getBytes());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+        RealConsole realConsole = new RealConsole(bufferedReader, null);
+        assertEquals("hello", realConsole.readUserInput());
+    }
 
-//    @Test
-//    public void printsAGivenStringToTheConsole() {
-//        InputStream input = new ByteArrayInputStream("0\n".getBytes());
-//        OutputStream output = new ByteArrayOutputStream();
-//        PrintStream printedOutput = new PrintStream(output);
-//        RealConsole realConsole = new RealConsole(input, printedOutput);
-//        realConsole.print("hey");
-//        assertEquals("hey\n", output.toString());
-//    }
+    @Test
+    public void printsAGivenStringToTheConsole() {
+        InputStream input = new ByteArrayInputStream("0\n".getBytes());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+        OutputStream output = new ByteArrayOutputStream();
+        PrintStream printedOutput = new PrintStream(output);
+        RealConsole realConsole = new RealConsole(bufferedReader, printedOutput);
+        realConsole.print("hey");
+        assertEquals("hey\n", output.toString());
+    }
 
     @Test(expected=UncheckedIOException.class)
     public void throwsAnExceptionForBadInput() throws IOException {
         InputStream input = new ByteArrayInputStream("hello\n".getBytes());
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
         bufferedReader.close();
-        RealConsole realConsole = new RealConsole(bufferedReader, input, null);
+        RealConsole realConsole = new RealConsole(bufferedReader, null);
         assertEquals("hello", realConsole.readUserInput());
     }
 }
