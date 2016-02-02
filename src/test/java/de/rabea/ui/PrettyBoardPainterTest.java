ac7830c4e3c5ff749c4c5881afb647845828b820
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-02 17:27:51 +0000

    adds test for 4x4 board

--- src/test/java/de/rabea/ui/PrettyBoardPainterTest.java
+++ src/test/java/de/rabea/ui/PrettyBoardPainterTest.java
@@ -1,7 +1,6 @@
 package de.rabea.ui;
 
 import de.rabea.game.Board;
-import org.junit.Before;
 import org.junit.Test;
 
 import static de.rabea.game.Mark.O;
@@ -9,15 +8,10 @@ import static de.rabea.game.Mark.X;
 import static org.junit.Assert.assertEquals;
 
 public class PrettyBoardPainterTest {
-    private Board board;
-
-    @Before
-    public void setup() {
-        board = new Board(3);
-    }
 
     @Test
-    public void displaysBoardWithMarksInDifferentColoursAndDashedLines() {
+    public void displays3x3BoardWithMarksInDifferentColoursAndDashedLines() {
+        Board board = new Board(3);
         board.placeMark(1, X);
         board.placeMark(3, O);
         PrettyBoardPainter prettyBoardPainter = new PrettyBoardPainter();
@@ -31,5 +25,28 @@ public class PrettyBoardPainterTest {
                 prettyBoardPainter.drawBoard(board)
 
         );
+    }
+
+    @Test
+    public void displays4x4BoardWithColourfulMarksAndDashedLines() {
+        Board board = new Board(4);
+        board.placeMark(1, X);
+        board.placeMark(4, O);
+        board.placeMark(15,X);
+        PrettyBoardPainter prettyBoardPainter = new PrettyBoardPainter();
+        assertEquals(
+                "\n| 1 | \u001B[34mX\u001B[0m | 3 | 4 | \n" +
+                        " -----------\n" +
+                        "| \u001B[31mO\u001B[0m | 6 | 7 | 8 | \n" +
+                        " -----------\n" +
+                        "| 9 | 10 | 11 | 12 | \n" +
+                        " -----------\n" +
+                        "| 13 | 14 | 15 | \u001B[34mX\u001B[0m |\n",
+
+                prettyBoardPainter.drawBoard(board)
+
+        );
+
+
     }
 }
