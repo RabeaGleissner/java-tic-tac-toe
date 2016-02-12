fdde353fedc98249bd66afcb189b5831baa2cc66
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-12 11:27:57 +0000

    delete unused imports

--- src/test/java/de/rabea/player/UnbeatableComputerPlayerTest.java
+++ src/test/java/de/rabea/player/UnbeatableComputerPlayerTest.java
@@ -1,13 +1,10 @@
 package de.rabea.player;
 
 import de.rabea.game.Board;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 import static de.rabea.game.Mark.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class UnbeatableComputerPlayerTest {
     UnbeatableComputerPlayer unbeatableComputerPlayer = new UnbeatableComputerPlayer(O);
