82a08262061c7ec8f5ae927d86ab42bce12a58a3
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-16 19:53:25 +0000

    adds main

---
+++ src/main/java/de/rabea/gui/Main.java
@@ -0,0 +1,21 @@
+package de.rabea.gui;
+
+import de.rabea.game.Board;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Board board = new Board(3);
+        BoardView boardView = new BoardView(board);
+        primaryStage.setScene(new Scene(boardView.draw()));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
