f4c18d0c9f69665a955e2ae1b5fede5b02fbd27b
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-16 10:33:21 +0000

    removes assertions and helper method

--- src/test/java/de/rabea/player/ComputerPlayerSpeedTest.java
+++ src/test/java/de/rabea/player/ComputerPlayerSpeedTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static de.rabea.game.Mark.X;
-import static org.junit.Assert.assertTrue;
 
 public class ComputerPlayerSpeedTest {
 
@@ -18,18 +17,11 @@ public class ComputerPlayerSpeedTest {
 
     @Test(timeout=800)
     public void measureSpeedOfFirstMoveOn3x3Board() {
-        assertTrue(800 > durationOfFirstComputerMove(new Board(3)));
+        computer.getPosition(new Board(3));
     }
 
     @Test(timeout=2999)
     public void measureSpeedOfFirstMoveOn4x4Board() {
-        assertTrue(2999 > durationOfFirstComputerMove(new Board(4)));
-    }
-
-    private long durationOfFirstComputerMove(Board board) {
-        long startTime = System.nanoTime();
-        computer.getPosition(board);
-        long endTime = System.nanoTime();
-        return (endTime - startTime)/1000000;
+        computer.getPosition(new Board(4));
     }
 }
