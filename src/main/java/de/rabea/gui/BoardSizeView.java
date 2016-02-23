702288e6831a42e3f66246c6d8894be6bc3ea25c
Rabea Gleissner <rabeagleissner@gmail.com>
2016-02-23 15:45:17 +0000

    reduces duplication

--- src/main/java/de/rabea/gui/BoardSizeView.java
+++ src/main/java/de/rabea/gui/BoardSizeView.java
@@ -11,26 +11,22 @@ public class BoardSizeView {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
         gridPane.add(new Label("Please select a board size:"), 0, 0);
-        JavaFXButton button3x3 = create3x3Button(guiApp);
-        JavaFXButton button4x4 = create4x4Button(guiApp);
-        gridPane.add(button3x3.getActualButton(), 1, 1);
-        gridPane.add(button4x4.getActualButton(), 2, 1);
+        addButtons(guiApp, gridPane);
         return gridPane;
     }
 
-    private JavaFXButton create4x4Button(GuiApp guiApp) {
-        JavaFXButton button4x4 = new JavaFXButton();
-        button4x4.setOnAction(new BoardSizeClickHandler(guiApp));
-        button4x4.setText("4x4 board");
-        button4x4.setId("4x4");
-        return button4x4;
+    private void addButtons(GuiApp guiApp, GridPane gridPane) {
+        JavaFXButton button3x3 = createButton(guiApp, "3x3");
+        JavaFXButton button4x4 = createButton(guiApp, "4x4");
+        gridPane.add(button3x3.getActualButton(), 1, 1);
+        gridPane.add(button4x4.getActualButton(), 2, 1);
     }
 
-    private JavaFXButton create3x3Button(GuiApp guiApp) {
-        JavaFXButton button3x3 = new JavaFXButton();
-        button3x3.setOnAction(new BoardSizeClickHandler(guiApp));
-        button3x3.setText("3x3 board");
-        button3x3.setId("3x3");
-        return button3x3;
+    private JavaFXButton createButton(GuiApp guiApp, String size) {
+        JavaFXButton button = new JavaFXButton();
+        button.setOnAction(new BoardSizeClickHandler(guiApp));
+        button.setText(size + " board");
+        button.setId(size);
+        return button;
     }
 }
