d74d5b852c87a8623c6071cfc6eb794d8a75754c
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-01 11:35:36 +0000

    removes duplicate tests from BoardTest

--- src/test/java/de/rabea/game/BoardTest.java
+++ src/test/java/de/rabea/game/BoardTest.java
@@ -15,53 +15,25 @@ import static org.junit.Assert.assertFalse;
 public class BoardTest {
 
     @Test
-    public void boardIsEmpty3x3() {
-        Board emptyBoard = new Board(3);
-        assertFalse(emptyBoard.isFull());
-    }
-
-    @Test
-    public void boardIsFull3x3() {
+    public void boardIsFull() {
         Board fullBoard = full3x3BoardNoWinner();
         assertTrue(fullBoard.isFull());
     }
 
     @Test
-    public void isChosenPositionStillFreeOn3x3() {
+    public void positionIsAvailable() {
         Board emptyBoard = new Board(3);
         assertEquals(true, emptyBoard.isPositionAvailable(1));
     }
 
     @Test
-    public void markedPositionIsNoLongerAvailableOn3x3() {
-        Board board = new Board(3);
-        Board nextBoard = board.placeMark(1, X);
+    public void positionIsNoLongerAvailable() {
+        Board nextBoard = new Board(3).placeMark(1, X);
         assertFalse(nextBoard.isPositionAvailable(1));
     }
 
     @Test
-    public void markedPositionIsNoLongerAvailableOn4x4() {
-        Board board = new Board(4);
-        Board nextBoard = board.placeMark(1, X);
-        assertFalse(nextBoard.isPositionAvailable(1));
-    }
-
-    @Test
-    public void knowsWhenPositionIsUnavailableOn3x3() {
-        Board board = new Board(3);
-        int positionNotOnBoard = 9;
-        assertFalse(board.isPositionAvailable(positionNotOnBoard));
-    }
-
-    @Test
-    public void knowsWhenPositionIsAvailableOn4x4() {
-        Board board = new Board(4);
-        int positionOnBoard = 9;
-        assertTrue(board.isPositionAvailable(positionOnBoard));
-    }
-
-    @Test
-    public void listsAllEmptyPositionsFor3x3() {
+    public void listsAllEmptyPositionsFor3x3Board() {
         Board board = new Board(EMPTY, X, O,
                                 X, EMPTY, EMPTY,
                                 O, EMPTY, EMPTY);
@@ -69,7 +41,7 @@ public class BoardTest {
     }
 
     @Test
-    public void listsAllEmptyPositionsFor4x4() {
+    public void listsAllEmptyPositionsFor4x4Board() {
         Board board = new Board(EMPTY, X, O, X,
                                 EMPTY, EMPTY, O, EMPTY,
                                 EMPTY, EMPTY, EMPTY, EMPTY,
@@ -79,136 +51,65 @@ public class BoardTest {
     }
 
     @Test
-    public void indexOfLastCellInEachRowOn3x3() {
+    public void indexOfLastCellInEachRow() {
         Board board = new Board(3);
-        List<Integer> cells = new ArrayList<>(asList(2, 5, 8));
-        assertEquals(cells, board.indexOfLastCellPerRow());
+        assertEquals(new ArrayList<>(asList(2, 5, 8)), board.indexOfLastCellPerRow());
     }
 
     @Test
-    public void indexOfLastCellOfBoardOn3x3() {
+    public void indexOfLastCellOfBoard() {
         Board board = new Board(3);
         assertEquals(8, board.indexOfLastCell());
     }
 
     @Test
-    public void widthAndHeightOf3x3() {
+    public void widthAndHeightOf3x3Board() {
         Board board = new Board(3);
         assertEquals(3, board.getDimension());
     }
 
     @Test
-    public void isEndOfLastRowFor3x3() {
+    public void knowsIfIndexIsLastCellOfBoard() {
         Board board = new Board(3);
         assertTrue(board.isIndexOfLastCell(8));
         assertFalse(board.isIndexOfLastCell(7));
     }
 
     @Test
-    public void isEndOfLastRowFor4x4() {
-        Board board = new Board(4);
-        assertTrue(board.isIndexOfLastCell(15));
-        assertFalse(board.isIndexOfLastCell(7));
+    public void boardHasWinnerWithHorizontalLine() {
+        assertTrue(horizontalsWinning3x3Board().gameOver());
     }
 
     @Test
-    public void gameOverWithoutWinnerOn3x3() {
-        Board board = full3x3BoardNoWinner();
-        assertTrue(board.gameOver());
-        assertFalse(board.hasWinner());
+    public void boardHasWinnerWithVerticalLine() {
+        assertTrue(verticalsWinning3x3Board().hasWinner());
     }
 
     @Test
-    public void gameOverWithoutWinnerOn4x4() {
-        Board board = full4x4BoardNoWinner();
-        assertTrue(board.gameOver());
-        assertFalse(board.hasWinner());
+    public void boardHasWinnerWithDiagonalLine() {
+        assertTrue(diagonalsWinning3x3Board().hasWinner());
     }
 
     @Test
-    public void gameOverWithWinnerOn3x3() {
-        Board board = horizontalsWinning3x3Board();
-        assertTrue(board.gameOver());
+    public void playerXIsWinner() {
+        assertEquals(X, verticalsWinning3x3Board().winningPlayerMark());
     }
 
     @Test
-    public void gameOverWithWinnerOn4x4() {
-        Board board = horizontalsWinning4x4Board();
-        assertTrue(board.gameOver());
+    public void gameIsDrawn() {
+        assertTrue(full3x3BoardNoWinner().isDrawn());
     }
 
     @Test
-    public void verticalWinningCombinationOn3x3() {
-        Board board = verticalsWinning3x3Board();
-        assertTrue(board.hasWinner());
+    public void nullWhenNoWinner() {
+        assertEquals(null, full3x3BoardNoWinner().winningPlayerMark());
     }
 
     @Test
-    public void verticalWinningCombinationOn4x4() {
-        Board board = verticalsWinning4x4Board();
-        assertTrue(board.hasWinner());
-    }
-
-    @Test
-    public void horizontalWinningCombinationOn3x3() {
-        Board board = horizontalsWinning3x3Board();
-        assertTrue(board.hasWinner());
-    }
-
-    @Test
-    public void horizontalWinningCombinationOn4x4() {
-        Board board = horizontalsWinning4x4Board();
-        assertTrue(board.hasWinner());
-    }
-
-    @Test
-    public void diagonalWinningCombinationOn3x3() {
-        Board board = diagonalsWinning3x3Board();
-        assertTrue(board.hasWinner());
-    }
-
-    @Test
-    public void diagonalWinningCombinationOn4x4() {
-        Board board = diagonalsWinning4x4Board();
-        assertTrue(board.hasWinner());
-    }
-
-    @Test
-    public void playerXIsWinnerOn3x3() {
-        Board boardWithWinnerX = verticalsWinning3x3Board();
-        assertEquals(X, boardWithWinnerX.winningPlayerMark());
-    }
-
-    @Test
-    public void gameIsDrawnOn3x3() {
-        Board drawnGameBoard = full3x3BoardNoWinner();
-        assertTrue(drawnGameBoard.isDrawn());
-    }
-
-    @Test
-    public void nullWhenNoWinnerOn3x3() {
-        Board fullBoardNoWinner = full3x3BoardNoWinner();
-        assertEquals(null, fullBoardNoWinner.winningPlayerMark());
-    }
-
-    @Test
-    public void placesMarkOnNew3x3Board() {
+    public void placesMarkOnNewBoard() {
         Board board = new Board(3);
         board.placeMark(1, X);
         assertEquals(9, board.emptyCells().size());
-    }
-
-    @Test
-    public void placesMarkOnNew4x4Board() {
-        Board board = new Board(4);
-        board.placeMark(1, X);
-        assertEquals(16, board.emptyCells().size());
-    }
-
-    @Test
-    public void knowsThatThereIsAWinnerOn4x4() {
-        Board board = backwardsDiagonalsWinning4x4Board();
-        assertTrue(board.hasWinner());
     }
 
     @Test
@@ -227,56 +128,25 @@ public class BoardTest {
                 O, O, X,
                 X, O, X);
     }
-    private Board full4x4BoardNoWinner() {
-        return new Board(
-                X, X, O, O,
-                O, O, X, X,
-                X, O, X, O,
-                X, O, X, O);
-    }
+
     private Board horizontalsWinning3x3Board() {
         return new Board(
                 X, X, X,
                 EMPTY, EMPTY, EMPTY,
                 EMPTY, O, O);
     }
-    private Board horizontalsWinning4x4Board() {
-        return new Board(
-                X, X, X, X,
-                EMPTY, EMPTY, EMPTY,O,
-                EMPTY, O, O, O,
-                X, O, X, O);
-    }
+
     private Board verticalsWinning3x3Board() {
         return new Board(
                 X, X, O,
                 X, O, O,
                 X, O, X);
     }
-    private Board verticalsWinning4x4Board(){
-        return new Board(X, X, O, EMPTY,
-                         X, O, O, EMPTY,
-                         X, O, X, O,
-                         X, EMPTY, EMPTY, EMPTY);
-    }
+
     private Board diagonalsWinning3x3Board() {
         return new Board(
                 X, X, O,
                 EMPTY, X, EMPTY,
                 EMPTY, O, X);
-    }
-    private Board diagonalsWinning4x4Board() {
-        return new Board(
-                X,     X,     O,     O,
-                EMPTY, X,     EMPTY, O,
-                EMPTY, O,     X,     O,
-                EMPTY, EMPTY, EMPTY, X);
-    }
-    private Board backwardsDiagonalsWinning4x4Board() {
-        return new Board(
-                X, O, O, X,
-                O, X, X, O,
-                O, X, EMPTY, EMPTY,
-                X, EMPTY, EMPTY, EMPTY);
     }
 }
