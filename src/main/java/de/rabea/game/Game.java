ad4df17659458b8ad09c9a11cd795811ec60ce4a
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-03 11:50:26 +0000

    removes notCurrentPlayer variable

--- src/main/java/de/rabea/game/Game.java
+++ src/main/java/de/rabea/game/Game.java
@@ -6,29 +6,28 @@ public class Game {
     private final Player player1;
     private final Player player2;
     private Player currentPlayer;
-    private Player notCurrentPlayer;
 
     public Game(UserInterface userInterface, Player player1, Player player2) {
         this.userInterface = userInterface;
         this.player1 = player1;
         this.player2 = player2;
         this.currentPlayer = player1;
-        this.notCurrentPlayer = player2;
     }
 
     public void play(Board board) {
         userInterface.displayBoard(board, currentPlayer);
         while (gameIsNotOver(board) && currentPlayer.hasMove()) {
             board = playOneRound(currentPlayer, board);
-            switchPlayer();
+            switchPlayer(board);
             userInterface.displayBoard(board, currentPlayer);
         }
         finishGame(board);
     }
 
-    private void switchPlayer() {
-        currentPlayer = currentPlayer == player1 ? player2 : player1;
-        notCurrentPlayer = notCurrentPlayer == player1 ? player2 : player1;
+    private void switchPlayer(Board board) {
+        if (gameIsNotOver(board)) {
+            currentPlayer = currentPlayer == player1 ? player2 : player1;
+        }
     }
 
     private Board playOneRound(Player player, Board board) {
@@ -37,18 +36,18 @@ public class Game {
 
     private void finishGame(Board board) {
         userInterface.displayBoard(board, currentPlayer);
-        userInterface.announceGameEnd(notCurrentPlayer.mark(), board);
+        userInterface.announceGameEnd(currentPlayer.mark(), board);
     }
 
     private boolean gameIsNotOver(Board board) {
        return !board.gameOver();
     }
 
-    public Player getPlayer1() {
+    Player getPlayer1() {
         return this.player1;
     }
 
-    public Player getPlayer2() {
+    Player getPlayer2() {
         return this.player2;
     }
 }
